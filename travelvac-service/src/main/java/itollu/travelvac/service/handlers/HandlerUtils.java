fba7f281c4bb43dad957b360b69376424c491ab0
ilia-tolliu <ilia.tolliu@gmail.com>
2026-01-03 12:53:28 +0100

    extract helper to write JSON body

--- travelvac-service/src/main/java/itollu/travelvac/service/handlers/HandlerUtils.java
+++ travelvac-service/src/main/java/itollu/travelvac/service/handlers/HandlerUtils.java
@@ -1,5 +1,7 @@
 package itollu.travelvac.service.handlers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
@@ -9,5 +11,15 @@ public class HandlerUtils {
 
   static void applyContentTypeJson(HttpServerExchange exchange) {
     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, APPLICATION_JSON);
+  }
+
+  static void writeJsonBody(Object body, HttpServerExchange exchange, JsonMapper json) {
+    applyContentTypeJson(exchange);
+    try {
+      String jsonResponse = json.writeValueAsString(body);
+      exchange.getResponseSender().send(jsonResponse);
+    } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+    }
   }
 }
