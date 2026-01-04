9174af5a9e8b33affe5fcb16302b24e614210dff
ilia-tolliu <ilia.tolliu@gmail.com>
2026-01-04 14:03:49 +0100

    add risk service

---
+++ travelvac-service/src/main/java/itollu/travelvac/service/core/Risk.java
@@ -0,0 +1,8 @@
+package itollu.travelvac.service.core;
+
+public record Risk(
+        String infectionCode,
+        String infectionTitle,
+        String description
+) {
+}
---
+++ travelvac-service/src/main/java/itollu/travelvac/service/core/RiskRepository.java
@@ -0,0 +1,7 @@
+package itollu.travelvac.service.core;
+
+import java.util.List;
+
+public interface RiskRepository {
+    List<Risk> getRisks(CountryCode countryCode);
+}
