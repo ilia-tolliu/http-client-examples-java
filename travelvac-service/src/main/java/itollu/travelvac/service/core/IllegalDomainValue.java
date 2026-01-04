660ebd64f21dd89a02d4035b1a1db3c912b8881e
ilia-tolliu <ilia.tolliu@gmail.com>
2026-01-04 14:01:53 +0100

    add country code parser

---
+++ travelvac-service/src/main/java/itollu/travelvac/service/core/IllegalDomainValue.java
@@ -0,0 +1,17 @@
+package itollu.travelvac.service.core;
+
+public class IllegalDomainValue extends RuntimeException {
+
+    private final String expectedValueDescription;
+
+    private final String actualValue;
+
+    public IllegalDomainValue(String expectedValueDescription, String actualValue) {
+        var message = "Expected %s but got [%s]"
+                .formatted(expectedValueDescription, actualValue);
+        super(message);
+
+        this.actualValue = actualValue;
+        this.expectedValueDescription = expectedValueDescription;
+    }
+}
