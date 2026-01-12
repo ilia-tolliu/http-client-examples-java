533abe44249c723c28132e525cb291f9e14e2870
Ilia Tolliu <ilia.tolliu@etraveligroup.com>
2026-01-12 17:05:19 +0100

    fix metrics server: add content type

--- travelvac-client-monitoring/src/main/java/travelvac/client/monitoring/MetricsServer.java
+++ travelvac-client-monitoring/src/main/java/travelvac/client/monitoring/MetricsServer.java
@@ -7,25 +7,36 @@ import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MetricsServer {
-    public static final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
 
-    public Thread run() {
-        try {
-            var server = HttpServer.create(new InetSocketAddress(8081), 0);
-            server.createContext("/prometheus", httpExchange -> {
-                var response = registry.scrape();
-                httpExchange.sendResponseHeaders(200, response.getBytes().length);
-                try (var os = httpExchange.getResponseBody()) {
-                    os.write(response.getBytes());
-                }
-            });
+  private static final Logger LOG = LoggerFactory.getLogger(MetricsServer.class);
 
-            var serverThread = new Thread(server::start);
-            serverThread.start();
-            return serverThread;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+  private static final int PORT = 9990;
+
+  public static final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+  public Thread run() {
+    try {
+      var server = HttpServer.create(new InetSocketAddress(PORT), 0);
+      server.createContext("/metrics", httpExchange -> {
+        var response = registry.scrape();
+        httpExchange.getResponseHeaders().add("Content-Type", "text/plain; version=0.0.4");
+        httpExchange.sendResponseHeaders(200, response.getBytes().length);
+        try (var os = httpExchange.getResponseBody()) {
+          os.write(response.getBytes());
         }
+      });
+
+      var serverThread = new Thread(server::start);
+      serverThread.start();
+      LOG.info("Metrics server started on port {}", PORT);
+
+      return serverThread;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
